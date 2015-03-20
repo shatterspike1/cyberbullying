2a8cefba84dd990bece82330a995d3be3d7b63e6
shatterspike1 <stupidinternet303@gmail.com>
2015-03-20 16:48:02 -0600

    added Survey skeleton files, added index.md

---
+++ contents/js/survey/Instructions.js
@@ -0,0 +1,11 @@
+var Instructions = react.createClass({ displayName: 'Instructions',
+    render: function (){
+        var self = this
+        
+        return (
+            <div className = "stuff">
+                //Enter stuff here
+            </div>
+        )
+    }
+})

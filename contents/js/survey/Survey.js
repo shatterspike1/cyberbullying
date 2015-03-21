46fa4a4868b8448455c26833991e6d7bfd49946c
shatterspike1 <stupidinternet303@gmail.com>
2015-03-20 20:40:33 -0600

    Made dropdown menus for Survey, added Next Example button, still dumb

--- contents/js/survey/Survey.js
+++ contents/js/survey/Survey.js
@@ -24,15 +24,53 @@ var Survey = React.createClass({ displayName: 'Survey',
                 <div className="firstQuestion row">
                     <p>Is there any cyberaggressive behavior in the online posts? Mark yes if there is at least one negative word/comment and or content with intent to harm someone or others.</p>
                     <div className="p row">
+                        <select value="">
+                            <option value="Yes">Yes</option>
+                            <option value="No">No</option>
+                            <option value=""></option>
+                        </select>
+                    </div>
+                </div>
+                <div className="secondQuestion row">
+                    <p>Is there any cyerbullying in the online post? Mark yes if there are negative words and or comment with intent to harm someone or other, and the posts include two or more repeated negativity against a victim that cannot easily defend him or herself</p>
+                        <div className="p row">
+                            <select value="">
+                                <option value="Yes">Yes</option>
+                                <option value="No">No</option>
+                                <option value=""></option>
+                            </select>
+                        </div>
+                </div>
+                <div className="next row">
+                    <button>Next Example</button>
+                </div>
+            </div>
+        )
+    }
+
+})
+
+/*
+var Survey = React.createClass({ displayName: 'Survey',
+
+    render: function(){
+        return (
+            <div className="survey">
+                <div className="firstQuestion row">
+                    <h6>Is there any cyberaggressive behaviour in the online posts? Mark yes if there is at least one negative word/comment and or content with intent to harm someone or others.</h6>
+                    <div className="p row">
                         <button>{'Yes'}</button>
                         <button>{'No'}</button>
                     </div>
                 </div>
                 <div className="secondQuestion row">
-                    <p>Is there any cyerbullying in the online post? Mark yes if there are negative words and or comment with intent to harm someone or other, and the posts include two or more repeated negativity against a victim that cannot easily defend him or herself</p>
+                    <h6>Is there any cyerbullying in the online post? Mark yes if there are negative words and or comment with intent to harm someone or other, and the posts include two or more repeated negativity against a victim that cannot easily defend him or herself</h6>
                     <div className="q row">
-                        <button>{'Yes'}</button>
-                        <button>{'No'}</button>
+                        <select value="">
+                            <option value="Yes">Yes</option>
+                            <option value="No">No</option>
+                            <option value=""></option>
+                        </select>
                     </div>
                 </div>
             </div>
@@ -40,3 +78,4 @@ var Survey = React.createClass({ displayName: 'Survey',
     }
 
 })
+*/

f4bdbe810b1082590fadb7729ec16a04101d3b51
Samuel Tobey <Samuel.P.Tobey@colorado.edu>
2015-03-20 19:51:40 -0600

    ideas in comments

--- contents/js/survey/Overview.js
+++ contents/js/survey/Overview.js
@@ -8,19 +8,29 @@ var Overview = React.createClass({ displayName: 'Overview',
         //add stuff here
     },
     
+    // placeholder function for handling some click event
+    /*
+    onSomethingClicked: function(profile_id) {
+        var post = _.find(this.state.data, {profile_owner_id: profile_id})
+        this.refs.page.setState({page: post})
+        this.refs.survey.setState({page: post})
+    },
+    */
+    // <Page ref = "page"/>
+    
     render: function() {
         var self = this
         
         return (
             <div className= 'Overview'>
                 <div className = 'InstructionView three columns'>
-                <h2>Oh, hello!</h2>
+                    <h2>Instructions Stuff</h2>
                 </div>
                 <div className = 'PageView six columns'>
-                <h2>Over here!</h2>
+                    <h2>Page Stuff</h2>
                 </div>
                 <div id = 'survey' className = 'SurveyView'>
-                    <Survey ref ='survey'/>
+                    <Survey ref = "survey"/>
                 </div>
             </div>
         )

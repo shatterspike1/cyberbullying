abda9c1b2163e7bc46d57397fd4b44a0a60c9693
shatterspike1 <stupidinternet303@gmail.com>
2015-03-22 19:52:15 -0600

    Removed from repo

--- Makefile
+++
@@ -1,2 +0,0 @@
-all:
-	wintersmith build
--- build/js/app.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function(){
-    React.render(
-        <Overview url = 'data/cyberbullying_data.json.data' Survey store = 'data/survey_responses.json.data' />,
-        document.getElementById('content')
-    )
-})
